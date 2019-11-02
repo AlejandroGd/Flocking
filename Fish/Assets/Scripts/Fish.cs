5914cfa9d85a4ec162e706b6405fdaf08262e962
AlejandroGd <gonzalezdavila.a@gmail.com>
2019-11-02 02:10:41 +0000

    Fish tank addition + Bugs and debug functionality
    Added Fish Tank asset from unity store. Scene rebuilt.
    Loop for calculation of flock behaviour fixed.
    Added some debug functionality to see center of mass and specific behaviors for individual fishes.

--- Fish/Assets/Scripts/Fish.cs
+++ Fish/Assets/Scripts/Fish.cs
@@ -11,11 +11,21 @@ public class Fish : MonoBehaviour
     bool turning = false;
     Bounds b;
 
+    [SerializeField] bool fishDebug = false;
+
+    [SerializeField] MeshRenderer meshRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
         speed = Random.Range(myManager.MinSpeed, myManager.MaxSpeed);
-        b = new Bounds(myManager.transform.position, myManager.swimLimits * 3);
+        b = new Bounds(myManager.transform.position, myManager.swimLimits);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+     
     }
 
     // Update is called once per frame
@@ -24,31 +34,34 @@ public class Fish : MonoBehaviour
         avoidRadius = myManager.AvoidRadius;
         RaycastHit hit = new RaycastHit();
         Vector3 direction = Vector3.zero;
-
-        //Avoid obstacles
-
+                     
         
-
-        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, myManager.raycastLenght))
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, myManager.raycastLenght, LayerMask.GetMask("Obstacles")))
         {
+            //Avoid obstacles
             turning = true;
             Debug.DrawRay(this.transform.position, this.transform.forward * myManager.raycastLenght, Color.red);
             direction = Vector3.Reflect(this.transform.forward, hit.normal);
+            meshRenderer.material.color = Color.red;
         } 
         //Keep within boundaries
-        else if (!b.Contains(transform.position))
+        else
+       
+        if (IsOutsideSwimLimits())
         {
             turning = true;
-            direction = b.center - transform.position;
+            direction = myManager.transform.position - transform.position;
+            meshRenderer.material.color = Color.yellow;
         }
         else
         {
+            meshRenderer.material.color = Color.green;
             turning = false;
         }
 
         if (turning)
         {  
-            transform.rotation =                                            //Interpolation between:
+            transform.rotation =                                                    //Interpolation between:
                         Quaternion.Slerp(transform.rotation,                        //Quaternion value for the current rotation
                                          Quaternion.LookRotation(direction),        //Quaternion value for the target rotation
                                          myManager.RotationSpeed * Time.deltaTime); //by the rotation speed scaled to the time passed between frames
@@ -66,17 +79,55 @@ public class Fish : MonoBehaviour
         transform.position += transform.forward * speed * Time.deltaTime;    
     }
 
+    private bool IsOutsideSwimLimits()
+    {
+        Vector3 pos = transform.position;
+        Vector3 limit = myManager.swimLimits;
+        Vector3 fmPos = myManager.transform.position;
+
+        if (pos.x < fmPos.x - limit.x ||
+            pos.x > fmPos.x + limit.x ||
+            pos.y < fmPos.y - limit.y ||
+            pos.y > fmPos.y + limit.y ||
+            pos.z < fmPos.z - limit.z ||
+            pos.z > fmPos.z + limit.z)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        
+    }
+
     private void AddFlockBehaviourRules()
     {
         GameObject[] allFish = myManager.allFish;
 
         Vector3 centerOfMass = Vector3.zero;
-        Vector3 avoidVector = Vector3.zero;
-        float groupSpeed = 0.01f;
+        Vector3 avoidVector = Vector3.zero;        
         float neighbourDistance;
         int neighbourCount = 0;
+        float groupSpeed = 0.01f;
 
-        foreach(GameObject fish in allFish)
+        foreach (GameObject fish in allFish)
+        {
+            if (fish != this.gameObject)
+            {
+                neighbourDistance = Vector3.Distance(fish.transform.position, this.transform.position);
+                if (neighbourDistance <= myManager.NeighbourDistance)
+                {
+                    neighbourCount++;
+                }
+            }
+        }
+
+        neighbourCount += 0;
+        neighbourCount = 0;
+
+        foreach (GameObject fish in allFish)
         {
             if (fish != this.gameObject)
             {
@@ -95,21 +146,28 @@ public class Fish : MonoBehaviour
                     groupSpeed += f.speed;
                 }
             }
+        }
 
-            if (neighbourCount > 0)
+        if (neighbourCount > 0)
+        {
+            centerOfMass = (centerOfMass / neighbourCount);
+            if (fishDebug) DebugCenterOfMass(centerOfMass);
+            speed = groupSpeed / neighbourCount;
+               
+            Vector3 direction = centerOfMass + avoidVector - transform.position;
+            if (direction != Vector3.zero)
             {
-                centerOfMass = centerOfMass / neighbourCount + (myManager.target.transform.position - this.transform.position);
-                speed = groupSpeed / neighbourCount;
-
-                Vector3 direction = centerOfMass + avoidVector - transform.position;
-                if (direction != Vector3.zero)
-                {
-                    transform.rotation =                                            //Interpolation between:
-                        Quaternion.Slerp(transform.rotation,                        //Quaternion value for the current rotation
-                                         Quaternion.LookRotation(direction),        //Quaternion value for the target rotation
-                                         myManager.RotationSpeed * Time.deltaTime); //by the rotation speed scaled to the time passed between frames
-                }
+                transform.rotation =                                            //Interpolation between:
+                    Quaternion.Slerp(transform.rotation,                        //Quaternion value for the current rotation
+                                        Quaternion.LookRotation(direction),        //Quaternion value for the target rotation
+                                        myManager.RotationSpeed * Time.deltaTime); //by the rotation speed scaled to the time passed between frames
             }
         }
+        
+    }
+
+    private void DebugCenterOfMass(Vector3 position)
+    {
+        myManager.debugBall.transform.position = position;
     }
 }
--- Fish/Assets/Scripts/FlockManager.cs
+++ Fish/Assets/Scripts/FlockManager.cs
@@ -26,20 +26,24 @@ public class FlockManager : MonoBehaviour
     [Range(0.0f, 5.0f)]
     [SerializeField] float rotationSpeed; public float RotationSpeed { get => rotationSpeed; }
 
+
+    [SerializeField] public GameObject debugBall;
+
+
     // Start is called before the first frame update
     void Start()
     {        
         allFish = new GameObject[numFish];
-        for(int i=0; i<numFish;i++)
+        
+        for (int i=0; i<numFish;i++)
         {
             Vector3 position = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
                                       Random.Range(-swimLimits.y, swimLimits.y),
-                                      Random.Range(-swimLimits.z, swimLimits.z));
-            if (position.y < 0) position.y = 0; //No start position below the floor
+                                      Random.Range(-swimLimits.z, swimLimits.z));            
 
             allFish[i] = Instantiate(fishPrefab, position, Quaternion.identity);
             allFish[i].GetComponent<Fish>().myManager = this;
-            allFish[i].transform.parent = this.transform;           
+           // allFish[i].transform.parent = this.transform;           
         }       
     }
 
