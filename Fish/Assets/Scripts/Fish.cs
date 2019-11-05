7c45d04c41bcd5f3bdd5af9f58b5e59354e51807
AlejandroGd <gonzalezdavila.a@gmail.com>
2019-11-05 14:29:41 +0000

    Refactoring to add state machine
    The flock behaviour has been refactored in the FlockBehaviour state in order to get ready to implement the state machine.

--- Fish/Assets/Scripts/Fish.cs
+++ Fish/Assets/Scripts/Fish.cs
@@ -2,34 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Common functionality to all fishes independent of what state is active.
 public class Fish : MonoBehaviour
 {
     public FlockManager myManager;
-    float speed;
-    public float avoidRadius = 1.0f;
-    float raycastDistance = 1f;
+    float raycastDistance = 1f;   
 
-    bool turning = false;    
-
-    [SerializeField] bool fishDebug = false;
-
+    //Debug variables
+    [SerializeField] bool fishDebug = false; public bool FishDebug { get => fishDebug; }
     [SerializeField] MeshRenderer meshRenderer;
-
-    // Start is called before the first frame update
-    void Start()
+      
+    //Add tint to the fish mesh to identify specific behaviours
+    public void AddDebugColor(Color color)
     {
-        speed = Random.Range(myManager.MinSpeed, myManager.MaxSpeed);  
+        meshRenderer.material.color = color;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Cast a front, left and right rays to check if the fish needs to turn due to obstacles.
+    //If it dies, direction returns a vector with the direction the fish needs to turn to.
+    public bool IsTurningDueObstacles(ref Vector3 direction)
     {
-        avoidRadius = myManager.AvoidRadius;
-        raycastDistance = myManager.rayCastDistance;
+        bool turning = false;
         RaycastHit hit = new RaycastHit();
-        Vector3 direction = Vector3.zero;
+        raycastDistance = myManager.rayCastDistance;
 
-        
         if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, myManager.raycastLenght, LayerMask.GetMask("Obstacles")) ||
             Physics.Raycast(this.transform.position, this.transform.right, out hit, raycastDistance, LayerMask.GetMask("Obstacles")) ||
             Physics.Raycast(this.transform.position, -this.transform.right, out hit, raycastDistance, LayerMask.GetMask("Obstacles")))
@@ -37,55 +33,38 @@ public class Fish : MonoBehaviour
             //Avoid obstacles
             turning = true;
             if (myManager.FlockDebug)
-            {             
+            {
                 Debug.DrawRay(this.transform.position, this.transform.forward * myManager.raycastLenght, Color.red);
                 Debug.DrawRay(this.transform.position, this.transform.right * raycastDistance, Color.red);
                 Debug.DrawRay(this.transform.position, -this.transform.right * raycastDistance, Color.red);
                 meshRenderer.material.color = Color.red;
             }
             direction = Vector3.Reflect(this.transform.forward, hit.normal);
-            
-        }         
-        else if (IsOutsideSwimLimits())
+        }
+
+        return turning;
+    }
+
+    //Cast a front, left and right rays to check if the fish needs to turn due to being swimming out of limits.
+    //If it dies, direction contains the vector with the direction the fish needs to turn to.
+    public bool IsTurningDueSwimmingLimits(ref Vector3 direction)
+    {
+        bool turning = false;
+        if (IsOutsideSwimLimits())
         {
             //Keep within boundaries
             turning = true;
             direction = myManager.transform.position - transform.position;
             if (myManager.FlockDebug) meshRenderer.material.color = Color.yellow;
         }
-        else
-        {
-            //Normal Flock behaviour
-            if (myManager.FlockDebug) meshRenderer.material.color = Color.green;
-            turning = false;
-        }
-
-        if (turning)
-        {  
-            transform.rotation =                                                    //Interpolation between:
-                        Quaternion.Slerp(transform.rotation,                        //Quaternion value for the current rotation
-                                         Quaternion.LookRotation(direction),        //Quaternion value for the target rotation
-                                         myManager.RotationSpeed * Time.deltaTime); //by the rotation speed scaled to the time passed between frames
-        }
-        else
-        {
-            //Randomly change speed now and then so the fish don end swimming all at the same speed
-            if (Random.Range(0, 100) < 3)
-            {
-                speed = Random.Range(myManager.MinSpeed, myManager.MaxSpeed);
-            }
-
-            //Flock behaviour rules do not need to be added on every frame (too expensive)
-            if (Random.Range(0, 100) < 15) AddFlockBehaviourRules();
-        }
-                
-        transform.position += transform.forward * speed * Time.deltaTime;    
+        return turning;
     }
-
+    //Combines the fish tank limits with the flock one and determines if the fish is within the union.
+    //(both inside the fisk tank and in the flock swim limits)
     private bool IsOutsideSwimLimits()
     {
         Vector3 fishPos = transform.position;
-        SwimBoundary limit = myManager.GetSwimmingBoundary();        
+        SwimBoundary limit = myManager.GetSwimmingBoundary();
 
         if (fishPos.x < limit.minX ||
             fishPos.x > limit.maxX ||
@@ -99,67 +78,6 @@ public class Fish : MonoBehaviour
         else
         {
             return false;
-        }        
-    }
-
-    private void AddFlockBehaviourRules()
-    {
-        GameObject[] allFish = myManager.allFish;
-
-        Vector3 centerOfMass = Vector3.zero;
-        Vector3 avoidVector = Vector3.zero;        
-        float neighbourDistance;
-        int neighbourCount = 0;
-        float groupSpeed = 0.01f;
-
-        neighbourCount = 0;
-
-        foreach (GameObject fish in allFish)
-        {
-            if (fish != this.gameObject)
-            {
-                neighbourDistance = Vector3.Distance(fish.transform.position, this.transform.position);
-                if (neighbourDistance <= myManager.NeighbourDistance)
-                {
-                    centerOfMass += fish.transform.position;
-                    neighbourCount++;
-
-                    if (neighbourDistance < avoidRadius)
-                    {
-                        avoidVector += (this.transform.position - fish.transform.position).normalized * (avoidRadius - neighbourDistance);
-                    }
-
-                    Fish f = fish.GetComponent<Fish>();
-                    groupSpeed += f.speed;
-                }
-            }
         }
-
-        if (neighbourCount > 0)
-        {
-            centerOfMass = (centerOfMass / neighbourCount);
-            if (fishDebug) DebugCenterOfMass(centerOfMass);
-            speed = groupSpeed / neighbourCount;
-               
-            Vector3 direction = centerOfMass + avoidVector - transform.position;
-            if (direction != Vector3.zero)
-            {
-                transform.rotation =                                            //Interpolation between:
-                    Quaternion.Slerp(transform.rotation,                        //Quaternion value for the current rotation
-                                        Quaternion.LookRotation(direction),        //Quaternion value for the target rotation
-                                        myManager.RotationSpeed * Time.deltaTime); //by the rotation speed scaled to the time passed between frames
-            }
-        }
-        
-    }
-
-    private void DebugCenterOfMass(Vector3 position)
-    {
-        myManager.debugBall.transform.position = position;
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (fishDebug) Debug.Log("Touchning");
-    }
+    }    
 }
---
+++ Fish/Assets/Scripts/State Machine Behaviours/FlockBehaviour.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockBehaviour : BaseState
+{
+    Fish fish;
+    
+    //Flock behaviour variables
+    public float avoidRadius = 1.0f;
+
+    //Movement
+    float speed = 0f;
+    bool turning = false;
+    Vector3 direction = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fish = GetComponent<Fish>();
+        speed = Random.Range(fish.myManager.MinSpeed, fish.myManager.MaxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        avoidRadius = fish.myManager.AvoidRadius;
+        direction = Vector3.zero;
+        turning = false;
+
+        turning = fish.IsTurningDueObstacles(ref direction);
+        if (!turning) turning = fish.IsTurningDueSwimmingLimits(ref direction);
+
+        if (turning)
+        {
+            transform.rotation =                                                    //Interpolation between:
+                        Quaternion.Slerp(transform.rotation,                        //Quaternion value for the current rotation
+                                         Quaternion.LookRotation(direction),        //Quaternion value for the target rotation
+                                         fish.myManager.RotationSpeed * Time.deltaTime); //by the rotation speed scaled to the time passed between frames
+        }
+        else
+        {
+            //Normal Flock behaviour
+            if (fish.myManager.FlockDebug) fish.AddDebugColor(Color.green);
+
+            //Randomly change speed now and then so the fish don end swimming all at the same speed
+            if (Random.Range(0, 100) < 3)
+            {
+                speed = Random.Range(fish.myManager.MinSpeed, fish.myManager.MaxSpeed);
+            }
+
+            //Flock behaviour rules do not need to be added on every frame (too expensive)
+            if (Random.Range(0, 100) < 15) AddFlockBehaviourRules();
+        }
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void AddFlockBehaviourRules()
+    {
+        GameObject[] allFish = fish.myManager.allFish;
+
+        Vector3 centerOfMass = Vector3.zero;
+        Vector3 avoidVector = Vector3.zero;
+        float neighbourDistance;
+        int neighbourCount = 0;
+        float groupSpeed = 0.01f;
+
+        neighbourCount = 0;
+
+        foreach (GameObject fishGameObject in allFish)
+        {
+            if (fishGameObject != this.gameObject)
+            {
+                neighbourDistance = Vector3.Distance(fishGameObject.transform.position, this.transform.position);
+                if (neighbourDistance <= fish.myManager.NeighbourDistance)
+                {
+                    centerOfMass += fishGameObject.transform.position;
+                    neighbourCount++;
+
+                    if (neighbourDistance < avoidRadius)
+                    {
+                        avoidVector += (this.transform.position - fishGameObject.transform.position).normalized * (avoidRadius - neighbourDistance);
+                    }
+
+                    FlockBehaviour f = fishGameObject.GetComponent<FlockBehaviour>();
+                    groupSpeed += f.speed;
+                }
+            }
+        }
+
+        if (neighbourCount > 0)
+        {
+            centerOfMass = (centerOfMass / neighbourCount);            
+            speed = groupSpeed / neighbourCount;
+
+            Vector3 direction = centerOfMass + avoidVector - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation =                                            //Interpolation between:
+                    Quaternion.Slerp(transform.rotation,                        //Quaternion value for the current rotation
+                                        Quaternion.LookRotation(direction),        //Quaternion value for the target rotation
+                                        fish.myManager.RotationSpeed * Time.deltaTime); //by the rotation speed scaled to the time passed between frames
+            }
+        }
+    }
+}
+
